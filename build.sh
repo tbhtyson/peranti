660508c74d73a5f7f1ee9b59e3d6c32c4629192a
Tice Cason <05.acidic-spurns@icloud.com>
2026-07-21 00:52:08 -0500

    finally, a glfw windoe

--- build.sh
+++ build.sh
@@ -1,1 +1,13 @@
-gcc -o build/a.out $(cat compile_flags.txt) src/main.c
+#!/usr/bin/env bash
+set -e
+
+GLFW_INC="third_party/glfw/include"
+GLFW_LIB="third_party/glfw/build/src"
+
+CFLAGS="-Wall -Wextra -Wpedantic -Wshadow -Wconversion -I${GLFW_INC}"
+LDFLAGS="-L${GLFW_LIB} -lglfw3 -lm -ldl -lpthread -lX11"
+
+cmake -S third_party/glfw -B third_party/glfw/build -DBUILD_SHARED_LIBS=OFF -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF -DGLFW_BUILD_WAYLAND=OFF
+cmake --build third_party/glfw/build
+gcc $CFLAGS -c src/main.c -o build/main.o
+gcc build/main.o $LDFLAGS -o build/peranti
--- src/main.c
+++ src/main.c
@@ -3,8 +3,38 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 
+typedef struct App {
+  GLFWwindow *window;
+} App;
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
+void initWindow(App *pApp) {
+  glfwInit();
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+  pApp->window = glfwCreateWindow(WIDTH, HEIGHT, "Peranti 0", NULL, NULL);
+}
+void initVulkan(void) {
+}
+void mainLoop(App *pApp) {
+    while (!glfwWindowShouldClose(pApp->window)) {
+        glfwPollEvents();
+    }
+}
+void cleanup(App *pApp) {
+  glfwDestroyWindow(pApp->window);
+  glfwTerminate();
+}
+
 int main(void) {
   printf("the slopfest begins here babyyyy\n");
+  App app = {0};
+  initWindow(&app);
+  initVulkan();
+  mainLoop(&app);
+  cleanup(&app);
 
   return 0;
 }
