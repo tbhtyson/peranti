5c11ff4b0aacbfad07bd73ffdb350f49483966b8
Tice Cason <05.acidic-spurns@icloud.com>
2026-07-21 13:12:31 -0500

    some vulkan boiling plates

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ GLFW_INC="third_party/glfw/include"
 GLFW_LIB="third_party/glfw/build/src"
 
 CFLAGS="-Wall -Wextra -Wpedantic -Wshadow -Wconversion -I${GLFW_INC}"
-LDFLAGS="-L${GLFW_LIB} -lglfw3 -lm -ldl -lpthread -lX11"
+LDFLAGS="-L${GLFW_LIB} -lglfw3 -lm -ldl -lpthread -lX11 -lvulkan"
 
 cmake -S third_party/glfw -B third_party/glfw/build -DBUILD_SHARED_LIBS=OFF -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF -DGLFW_BUILD_WAYLAND=OFF
 cmake --build third_party/glfw/build
--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <vulkan/vulkan_core.h>
 #define GLFW_INCLUDE_VULKAN
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
@@ -5,10 +7,39 @@
 
 typedef struct App {
   GLFWwindow *window;
+  VkInstance instance;
 } App;
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
+
+void createInstance(App *pApp) {
+  VkApplicationInfo appInfo = {0};
+  appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  appInfo.pApplicationName = "Peranti";
+  appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+  appInfo.pEngineName = "No Engine";
+  appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+  appInfo.apiVersion = VK_API_VERSION_1_0;
+  appInfo.pNext = NULL;
+
+  uint32_t glfwExtensionCount = 0;
+  const char** glfwExtensions;
+
+  glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+  VkInstanceCreateInfo createInfo = {0};
+  createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  createInfo.pApplicationInfo = &appInfo;
+  createInfo.enabledExtensionCount = glfwExtensionCount;
+  createInfo.ppEnabledExtensionNames = glfwExtensions;
+  createInfo.enabledLayerCount = 0;
+  if (vkCreateInstance(&createInfo, NULL, &pApp->instance) != VK_SUCCESS) {
+    printf("createInstance failure!\n");
+    exit(1);
+  }
+}
+
 
 void initWindow(App *pApp) {
   glfwInit();
@@ -16,7 +47,8 @@ void initWindow(App *pApp) {
   glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
   pApp->window = glfwCreateWindow(WIDTH, HEIGHT, "Peranti 0", NULL, NULL);
 }
-void initVulkan(void) {
+void initVulkan(App *pApp) {
+  createInstance(pApp);
 }
 void mainLoop(App *pApp) {
     while (!glfwWindowShouldClose(pApp->window)) {
@@ -24,6 +56,7 @@ void mainLoop(App *pApp) {
     }
 }
 void cleanup(App *pApp) {
+  vkDestroyInstance(pApp->instance, NULL);
   glfwDestroyWindow(pApp->window);
   glfwTerminate();
 }
@@ -32,7 +65,7 @@ int main(void) {
   printf("the slopfest begins here babyyyy\n");
   App app = {0};
   initWindow(&app);
-  initVulkan();
+  initVulkan(&app);
   mainLoop(&app);
   cleanup(&app);
 
